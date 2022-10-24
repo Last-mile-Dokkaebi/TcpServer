0d0823e85633a12a62a7bf2ed9527dc403c9a9a3
star-sil <kimse9811@naver.com>
2022-10-24 23:35:05 +0900

    UPDATE Scooter로 응답 반환시 스쿠터 상태 정보 (주행 여부) 추가 반환
    -

--- src/main/java/com/example/TcpServer/scooter/ScooterDao.java
+++ src/main/java/com/example/TcpServer/scooter/ScooterDao.java
@@ -13,6 +13,7 @@ public class ScooterDao {
     private int speed;
     private double lat;
     private double lng;
+    private int altitude;
     private String pow;
     private int shock;
 
@@ -22,12 +23,13 @@ public class ScooterDao {
         soc = Integer.parseInt(scooterData.substring(6, 8));
         volt = stringToDouble(scooterData.substring(8, 12),3);
         current = scooterData.substring(12,20);
-        temp = Integer.parseInt(scooterData.substring(20, 22));
-        speed = Integer.parseInt(scooterData.substring(22, 26));
-        lat = stringToDouble(scooterData.substring(26, 34), 4);
-        lng = stringToDouble(scooterData.substring(34, 42), 4);
-        pow = scooterData.substring(42, 44);
-        shock = Integer.parseInt(scooterData.substring(44, 46));
+        temp = Integer.parseInt(scooterData.substring(20, 24));
+        speed = Integer.parseInt(scooterData.substring(24, 28));
+        lat = stringToDouble(scooterData.substring(28, 36), 4);
+        lng = stringToDouble(scooterData.substring(36, 44), 4);
+        altitude = Integer.parseInt(scooterData.substring(44, 48));
+        pow = scooterData.substring(48, 50);
+        shock = Integer.parseInt(scooterData.substring(50, 52));
     }
 
     private double stringToDouble(String str, int index) {
